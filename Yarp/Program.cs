153502527efd19deb115495871a1e644c505060c
Alexandros Peppas <apeppas@microsoft.com>
2024-06-17 22:56:51 +0100

    custom rate limiter on YARP service

--- Yarp/Program.cs
+++ Yarp/Program.cs
@@ -2,6 +2,8 @@ namespace Yarp;
 
 using System.Security.Cryptography.X509Certificates;
 
+using Microsoft.AspNetCore.RateLimiting;
+
 using Yarp.ReverseProxy.Configuration;
 using Yarp.ReverseProxy.LoadBalancing;
 using Yarp.ReverseProxy.Transforms;
@@ -40,13 +42,57 @@ public class Program
             {
                 builderCtx.AddRequestHeader("App2SugarHeader", "Sugar", append: true);
             })
-            .Services.AddSingleton<ILoadBalancingPolicy, ConsistentHashingPolicy>();
+            .Services
+                .AddSingleton<ILoadBalancingPolicy, ConsistentHashingPolicy>()
+                .AddRateLimiter(limiterOpt =>
+                {
+                    limiterOpt.AddFixedWindowLimiter("myCustomPolicy", opt =>
+                    {
+                        /*
+                         *  1. PermitLimit
+                            Description: Specifies the maximum number of permits (requests) allowed within each time window.
+                            Example: opt.PermitLimit = 4; means that up to 4 requests are allowed in each time window.
+
+                            2. Window
+                            Description: Defines the duration of the fixed window.
+                            Example: opt.Window = TimeSpan.FromSeconds(12); means that each time window is 12 seconds long.
+
+                            3. QueueProcessingOrder
+                            Description: Determines the order in which requests are processed when they are queued due to exceeding the rate limit.
+                            Options:
+                            QueueProcessingOrder.OldestFirst: Processes the oldest request in the queue first.
+                            QueueProcessingOrder.NewestFirst: Processes the newest request in the queue first.
+                            Example: opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst; means that when the rate limit is exceeded, the requests will be queued and processed in the order they arrived, oldest requests first.
+
+                            4. QueueLimit
+                            Description: Specifies the maximum number of requests that can be queued when the rate limit is exceeded.
+                            Example: opt.QueueLimit = 2; means that up to 2 requests can be queued if the rate limit is reached. Additional requests will be rejected until the queue has space.
+                         */
+                        opt.PermitLimit = 4;
+                        opt.Window = TimeSpan.FromSeconds(10);
+                        opt.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+                        opt.QueueLimit = 2;
+                    });
+
+                    limiterOpt.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+                    limiterOpt.OnRejected = async (context, cts) =>
+                    {
+                        cts.ThrowIfCancellationRequested();
+
+                        var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+                        logger.LogWarning($"Path {context.HttpContext.Request.Path} has been rate-limited");
+
+                        await context.HttpContext.Response.WriteAsync("Rate limit exceeded, please try again later", cts);
+                    };
+                });
 
         var app  = builder.Build();
 
         app.Logger.LogInformation("Application launched");
 
         app.MapGet("/api", () => "Hello World from YARP");
+
+        app.UseRateLimiter();
 
         app.MapReverseProxy();
 
@@ -59,6 +105,7 @@ public class Program
         {
             new RouteConfig
             {
+                RateLimiterPolicy = "myCustomPolicy",
                 RouteId = "route1",
                 ClusterId = "cluster1",
                 Match = new RouteMatch
